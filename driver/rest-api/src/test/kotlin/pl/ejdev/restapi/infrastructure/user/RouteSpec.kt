072c7b360a7d607db339d232d511b54328de032c
ewozniak <emil.wozniak.591986@gmail.com>
2023-05-06 11:19:14 +0200

    - add route spec

---
+++ driver/rest-api/src/test/kotlin/pl/ejdev/restapi/infrastructure/user/RouteSpec.kt
@@ -0,0 +1,54 @@
+package pl.ejdev.restapi.infrastructure.user
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.kotest.core.spec.style.FeatureSpec
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.statement.*
+import io.ktor.serialization.jackson.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.routing.*
+import io.ktor.server.testing.*
+import org.koin.core.context.startKoin
+import org.koin.test.KoinTest
+
+private fun Application.module() {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+    routing { route("/api") { users() } }
+}
+
+private val mapper = jacksonObjectMapper()
+internal suspend inline fun <reified T : Any> HttpResponse.response(check: T.() -> Unit): Unit = body<String>()
+    .let { mapper.readValue<T>(it) }
+    .let { check(it) }
+
+internal suspend fun request(testCase: suspend HttpClient.() -> Unit) {
+    testApplication {
+        application { module() }
+        testCase(client)
+    }
+}
+
+abstract class RouteSpec(
+    body: FeatureSpec.() -> Unit = {}
+) : FeatureSpec(), KoinTest, RouteSpecRoot {
+    init {
+        beforeSpec {
+            startKoin {
+                modules(module)
+            }
+        }
+        body()
+    }
+}
+
+internal interface RouteSpecRoot {
+    val module: org.koin.core.module.Module
+}
--- driver/rest-api/src/test/kotlin/pl/ejdev/restapi/infrastructure/user/UsersRouteSpec.kt
+++ driver/rest-api/src/test/kotlin/pl/ejdev/restapi/infrastructure/user/UsersRouteSpec.kt
@@ -1,26 +1,15 @@
 package pl.ejdev.restapi.infrastructure.user
 
 import arrow.core.raise.either
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import io.kotest.core.spec.style.FeatureSpec
 import io.kotest.matchers.shouldBe
-import io.ktor.client.call.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.http.ContentType.*
 import io.ktor.http.HttpStatusCode.Companion.OK
-import io.ktor.serialization.jackson.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.routing.*
-import io.ktor.server.testing.*
 import io.mockk.coEvery
 import io.mockk.mockk
-import org.koin.core.context.startKoin
+import org.koin.core.module.Module
 import org.koin.dsl.module
-import org.koin.test.KoinTest
 import pl.ejdev.restapi.domain.user.entities.CreateUserQueryResult
 import pl.ejdev.restapi.domain.user.entities.GetUserQueryResult
 import pl.ejdev.restapi.domain.user.usecases.CreateUserUseCase
@@ -34,110 +23,84 @@ private const val EMAIL = "john.snow@westeron.com"
 private const val STATUS = "ACTIVE"
 private val ROLES = setOf("USER", "ANON")
 
-private fun Application.module() {
-    install(ContentNegotiation) {
-        jackson {
-            enable(SerializationFeature.INDENT_OUTPUT)
-        }
-    }
-    routing { route("/api") { users() } }
-}
+object UsersRouteSpec : RouteSpec() {
+    private val getUserUseCase = mockk<GetUserUseCase>()
+    private val getAllUsersUseCase = mockk<GetAllUsersUseCase>()
+    private val createUserUseCase = mockk<CreateUserUseCase>()
 
-suspend inline fun <reified T : Any> HttpResponse.receive(): T {
-    val mapper = jacksonObjectMapper()
-    return body<String>()
-        .let(mapper::readValue)
-}
-
-class UsersRouteSpec : FeatureSpec({
-    val getUserUseCase = mockk<GetUserUseCase>()
-    val getAllUsersUseCase = mockk<GetAllUsersUseCase>()
-    val createUserUseCase = mockk<CreateUserUseCase>()
-    val getUserQueryResult = GetUserQueryResult(1, USERNAME, FIRST_NAME, LAST_NAME, EMAIL, STATUS, setOf())
-    beforeSpec {
-        startKoin { modules(module {
-            single { getUserUseCase }
-            single { getAllUsersUseCase }
-            single { createUserUseCase }
-        }) }
+    override val module: Module = module {
+        single { getUserUseCase }
+        single { getAllUsersUseCase }
+        single { createUserUseCase }
     }
-    feature("User routes") {
-        scenario("get users") {
-            coEvery { getAllUsersUseCase.handle(any()) } returns either { listOf(getUserQueryResult) }
-            testApplication {
-                application { module() }
-                client.get("/api/users"){
-                    headers { accept(ContentType.Application.Json) }
-                }.run {
-                    status shouldBe OK
-                    receive<List<GetUserQueryResult>>().run {
-                        get(0).run {
+    init {
+        val getUserQueryResult = GetUserQueryResult(1, USERNAME, FIRST_NAME, LAST_NAME, EMAIL, STATUS, ROLES)
+        val createUserQueryResult = CreateUserQueryResult(1, USERNAME, FIRST_NAME, LAST_NAME, EMAIL, STATUS, ROLES)
+        feature("User routes") {
+            scenario("get users") {
+                coEvery { getAllUsersUseCase.handle(any()) } returns either { listOf(getUserQueryResult) }
+                request {
+                    get("/api/users") { headers { accept(Application.Json) } }.run {
+                        status shouldBe OK
+                        response<List<GetUserQueryResult>> {
+                            get(0).run {
+                                id shouldBe 1
+                                username shouldBe USERNAME
+                                firstName shouldBe FIRST_NAME
+                                lastName shouldBe LAST_NAME
+                                email shouldBe EMAIL
+                                status shouldBe STATUS
+                                roles shouldBe ROLES
+                            }
+                        }
+                    }
+                }
+            }
+            scenario("get user by id") {
+                coEvery { getUserUseCase.handle(any()) } returns either { getUserQueryResult }
+                request {
+                    get("/api/users/1") { headers { accept(Application.Json) } }.run {
+                        status shouldBe OK
+                        response<GetUserQueryResult> {
                             id shouldBe 1
                             username shouldBe USERNAME
                             firstName shouldBe FIRST_NAME
                             lastName shouldBe LAST_NAME
                             email shouldBe EMAIL
                             status shouldBe STATUS
-                            roles shouldBe setOf()
+                            roles shouldBe ROLES
                         }
                     }
                 }
             }
-        }
-        scenario("get user by id") {
-            coEvery { getUserUseCase.handle(any()) } returns either {
-                getUserQueryResult
-            }
-            testApplication {
-                application { module() }
-                client.get("/api/users/1") {
-                    headers { accept(ContentType.Application.Json) }
-                }.run {
-                    status shouldBe OK
-                    receive<GetUserQueryResult>().run {
-                        id shouldBe 1
-                        username shouldBe USERNAME
-                        firstName shouldBe FIRST_NAME
-                        lastName shouldBe LAST_NAME
-                        email shouldBe EMAIL
-                        status shouldBe STATUS
-                        roles shouldBe setOf()
-                    }
-                }
-            }
-        }
-        scenario("create user") {
-            coEvery { createUserUseCase.handle(any()) } returns either {
-                CreateUserQueryResult(1, USERNAME, FIRST_NAME, LAST_NAME, EMAIL, STATUS, ROLES)
-            }
-            testApplication {
-                application { module() }
-                client.post("/api/users") {
-                    headers { contentType(ContentType.Application.Json) }
-                    setBody(
-                        """
-                        {
-                          "username": "john.snow",
-                          "firstName": "John",
-                          "lastName": "Snow",
-                          "email": "john.snow@westeron.com",
-                          "roles": ["USER", "ANON"]
+            scenario("create user") {
+                coEvery { createUserUseCase.handle(any()) } returns either { createUserQueryResult }
+                request {
+                    post("/api/users") {
+                        headers { contentType(Application.Json) }
+                        setBody(
+                            """ {
+                                    "username": "john.snow",
+                                    "firstName": "John",
+                                    "lastName": "Snow",
+                                    "email": "john.snow@westeron.com",
+                                    "roles": ["USER", "ANON"]
+                                 }""".trimIndent()
+                        )
+                    }.run {
+                        status shouldBe OK
+                        response<CreateUserQueryResult> {
+                            id shouldBe 1
+                            username shouldBe USERNAME
+                            firstName shouldBe FIRST_NAME
+                            lastName shouldBe LAST_NAME
+                            email shouldBe EMAIL
+                            status shouldBe STATUS
+                            roles shouldBe ROLES
                         }
-                    """.trimIndent()
-                    )
-                }.apply {
-                    status shouldBe OK
-                    receive<CreateUserQueryResult>().run {
-                        id shouldBe 1
-                        username shouldBe USERNAME
-                        firstName shouldBe FIRST_NAME
-                        lastName shouldBe LAST_NAME
-                        email shouldBe EMAIL
-                        status shouldBe STATUS
-                        roles shouldBe ROLES
                     }
                 }
             }
         }
     }
-}), KoinTest
+}
